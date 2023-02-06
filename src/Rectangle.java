86a92357210031d177c4af5a7db379755a21f96b
Belll5145 <Belll@wlhs.wlwv.k12.or.us>
2023-02-06 09:20:19 -0800

    Mutators done

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -32,13 +32,19 @@ public class Rectangle { //make public for CS A
 
 
     //accessors
+    /**
+     *
+     * @return the width of the rectangle
+     */
     public double getWidth(){
         return width;
     }
 
+
+
     /**
      *
-     * @return the width of the rectangle
+     * @return the height of the rectangle
      */
     public double getHeight(){
         return height;
@@ -46,8 +52,19 @@ public class Rectangle { //make public for CS A
 
     /**
      *
-     * @return the height of the rectangle
+     * @return the area of the rectangle
      */
+    public double getArea(){
+        return area;
+    }
+
+    /**
+     *
+     * @return the perimeter of the rectangle
+     */
+    public double getPerimeter(){
+        return perimeter;
+    }
 
 
     //mutators
@@ -55,10 +72,31 @@ public class Rectangle { //make public for CS A
         width = newWidth;
     }
 
+    /**
+     * Modifies the height of the rectangle
+     * @param newHeight
+     */
+    public void setHeight(double newHeight){
+        height = newHeight;
+    }
 
     //other methods
 
+    /**
+     * Calculates the area of the rectangle
+     *
+     */
+    public void calculateArea(){
+        area = height*width;
+    }
 
+    /**
+     * Calculates the perimeter of the rectangle
+     *
+     */
+    public void calculatePerimeter(){
+        perimeter = (height*2)+(width*2);
+    }
 
 
     //toString method
--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -1,7 +1,10 @@
 public class RectangleTester {
     public static void main(String[] args) {
         Rectangle rect1 = new Rectangle(7.45676, 394.488543);
+        double expectedPerimeter = 2*7.45676 + 2*394.488543;
         System.out.println(rect1);
+        rect1.calculatePerimeter();
+        System.out.println(rect1.getPerimeter() == expectedPerimeter);
 
     }
 }
